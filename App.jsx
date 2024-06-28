b0b2e41c186459aad23893e03af5e5139e933f2a
Lukman.kpi <lukman.mohammad@kpitechservices.com>
2024-06-28 18:35:18 +0530

    Added Home Laoding Indecator

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppNavigator from './src/Navigations/AppNavigator';
 import {PaperProvider} from 'react-native-paper';
 import {PasswordProvider} from './src/screens/PasswordContext/PasswordContext';
 import {collection, addDoc, getDocs} from 'firebase/firestore';
+import LoadingScreen from './src/components/LoadingScreen/LoadingScreen';
 
 // try {
 //   const docRef = await addDoc(collection(db, 'users'), {
@@ -35,6 +36,17 @@ import {collection, addDoc, getDocs} from 'firebase/firestore';
 // });
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
   return (
     <PasswordProvider>
       <PaperProvider>
---
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -0,0 +1,11 @@
+// LoadingScreen.js
+import React from 'react';
+import {View, ActivityIndicator} from 'react-native';
+
+const LoadingScreen = () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <ActivityIndicator size="large" color="#0000ff" />
+  </View>
+);
+
+export default LoadingScreen;
